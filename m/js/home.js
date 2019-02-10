b7a0a9b305f63c828bd7dadd1506ce9b1f87fa9d
shishuyue.ssy@gmail.com <shuyue.shi@mail.rayplus.net>
2019-02-10 16:48:39 +0800

    Z

--- m/js/home.js
+++ m/js/home.js
@@ -1,3 +1,13 @@
+function winScroll(){
+    if(document.body.scrollTop>0){
+        let lineScroll = document.getElementById("lineScroll")
+        lineScroll.style.display="none"
+    }else{
+        lineScroll.style.display="block"
+    }
+}   
+window.addEventListener('scroll',winScroll);
+
 let homelist = document.getElementById("homelist")
 updatalist("homelist",homelist)
 
