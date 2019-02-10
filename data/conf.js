2b03c1bf17b38cc1fe58032a0dd26a2a51f90dd8
shishuyue.ssy@gmail.com <shuyue.shi@mail.rayplus.net>
2019-02-10 11:37:22 +0800

    Z

--- data/conf.js
+++ data/conf.js
@@ -4,7 +4,7 @@ let SETTING={
 			"title": "THE BOTTLES 1",
 			"date": "2018.06.11",
 			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
-			"imgSrc": "data/img/1.png",
+			"imgSrc": "data/img/20190101.png",
 			"detailSrc": "data/img/1-1.png",
 			"like": 0
 		},
@@ -12,7 +12,7 @@ let SETTING={
 			"title": "THE BOTTLES 2",
 			"date": "2018.06.11",
 			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
-			"imgSrc": "data/img/2.gif",
+			"imgSrc": "data/img/20190102.png",
 			"detailSrc": "data/img/2-1.png",
 			"like": 0
 		},
@@ -20,7 +20,7 @@ let SETTING={
 			"title": "THE BOTTLES 3",
 			"date": "2018.06.11",
 			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
-			"imgSrc": "data/img/3.gif",
+			"imgSrc": "data/img/20190101.png",
 			"detailSrc": "data/img/1-1.png",
 			"like": 7
 		},
@@ -28,7 +28,7 @@ let SETTING={
 			"title": "THE BOTTLES 4",
 			"date": "2018.06.11",
 			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
-			"imgSrc": "data/img/4.gif",
+			"imgSrc": "data/img/20190102.png",
 			"detailSrc": "data/img/2-1.png",
 			"like": 2
 		},
@@ -36,7 +36,7 @@ let SETTING={
 			"title": "THE BOTTLES 5",
 			"date": "2018.06.11",
 			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
-			"imgSrc": "data/img/5.gif",
+			"imgSrc": "data/img/20190101.png",
 			"detailSrc": "data/img/1-1.png",
 			"like": 4
 		},
@@ -44,7 +44,7 @@ let SETTING={
 			"title": "THE BOTTLES 6",
 			"date": "2018.06.11",
 			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
-			"imgSrc": "data/img/6.gif",
+			"imgSrc": "data/img/20190102.png",
 			"detailSrc": "data/img/2-1.png",
 			"like": 0
 		}
