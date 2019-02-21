5da0ae5b18118405b1800b48c2ed3916f0ee74f7
511 <a511@511deMacBook-Pro.local>
2019-02-21 23:08:52 +0800

    A

--- data/conf.js
+++ data/conf.js
@@ -113,68 +113,198 @@ let SETTING={
 	],
 	"illustrationlist": [
 		{
-			"title": "THE BOTTLES 1",
-			"date": "2018.06.11",
+			"title": "SINGLE OR NOT",
+			"date": "2017.11.11",
 			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
-			"imgSrc": "data/img/20190101.png",
+			"imgSrc": "data/illustration-img/s01.jpg",
 			"detailSrc": [
-				"data/img/3-1.png",
-				"data/img/3-2.png"
+				"data/illustration-img/01.jpg",
 			],
-			"like": 5
+			"like": 98
 		},
 		{
-			"title": "THE BOTTLES 2",
-			"date": "2018.06.11",
+			"title": "MAJOR COLD",
+			"date": "2018.1.20",
 			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
-			"imgSrc": "data/img/20190102.png",
+			"imgSrc": "data/illustrtion-img/s02.jpg",
 			"detailSrc": [
-				"data/img/4-1.png",
-				"data/img/2-1.png"
+				"data/illustrtion-img/02-1.jpg",
+				"data/illustrtion-img/02-2.jpg"
 			],
 			"like": 1
 		},
 		{
-			"title": "THE BOTTLES 3",
-			"date": "2018.06.11",
+			"title": "ARBOR DAY",
+			"date": "2018.03.12",
 			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
-			"imgSrc": "data/img/20190101.png",
+			"imgSrc": "data/illustrtion-img/s03.jpg",
 			"detailSrc": [
-				"data/img/3-1.png",
-				"data/img/3-2.png"
+				"data/illustrtion-img/03-1.jpg",
+				"data/illustrtion-img/03-2.jpg"
 			],
 			"like": 7
 		},
 		{
-			"title": "THE BOTTLES 4",
-			"date": "2018.06.11",
+			"title": "WINWIN UED",
+			"date": "2018.06.13",
 			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
-			"imgSrc": "data/img/20190102.png",
+			"imgSrc": "data/illustrtion-img/s04.jpg",
 			"detailSrc": [
-				"data/img/4-1.png",
-				"data/img/2-1.png"
+				"data/illustrtion-img/04-1.jpg",
+				"data/illustrtion-img/04-2.jpg",
+				"data/illustrtion-img/04-3.jpg",
+				"data/illustrtion-img/04-4.jpg"
 			],
 			"like": 2
 		},
 		{
-			"title": "THE BOTTLES 5",
-			"date": "2018.06.11",
+			"title": "SUPERWIN",
+			"date": "2018.07.10",
 			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
-			"imgSrc": "data/img/20190101.png",
+			"imgSrc": "data/illustrtion-img/s05.jpg",
 			"detailSrc": [
-				"data/img/3-1.png",
-				"data/img/3-2.png"
+				"data/illustrtion-img/05-1.jpg",
+				"data/illustrtion-img/05-2.jpg",
+				"data/illustrtion-img/05-3.jpg",
+				"data/illustrtion-img/05-4.jpg",
+				"data/illustrtion-img/05-5.jpg",
+				"data/illustrtion-img/05-6.jpg",
+				"data/illustrtion-img/05-7.jpg"
 			],
 			"like": 4
 		},
 		{
-			"title": "THE BOTTLES 6",
-			"date": "2018.06.11",
+			"title": "SUPERWIN COMICS",
+			"date": "2018.08.20",
 			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
-			"imgSrc": "data/img/20190102.png",
+			"imgSrc": "data/illustrtion-img/s06.jpg",
 			"detailSrc": [
-				"data/img/4-1.png",
-				"data/img/2-1.png"
+				"data/illustrtion-img/06-1.jpg",
+				"data/illustrtion-img/06-2.jpg"
+			],
+			"like": 1
+		},
+		{
+			"title": "TRAVELING",
+			"date": "2018.09.17",
+			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
+			"imgSrc": "data/illustrtion-img/s07.jpg",
+			"detailSrc": [
+				"data/illustrtion-img/07-1.jpg",
+				"data/illustrtion-img/07-2.jpg",
+				"data/illustrtion-img/07-3.jpg",
+				"data/illustrtion-img/07-4.jpg"
+			],
+			"like": 1
+		},
+		{
+			"title": "THE BOTTLES",
+			"date": "2018.10.04",
+			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
+			"imgSrc": "data/illustrtion-img/s08.jpg",
+			"detailSrc": [
+				"data/illustrtion-img/08-1.jpg",
+				"data/illustrtion-img/08-2.jpg",
+				"data/illustrtion-img/08-3.jpg",
+				"data/illustrtion-img/08-4.jpg"
+			],
+			"like": 1
+		},
+		{
+			"title": "AVATARS",
+			"date": "2018.10.13",
+			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
+			"imgSrc": "data/illustrtion-img/s09.jpg",
+			"detailSrc": [
+				"data/illustrtion-img/09-1.jpg",
+				"data/illustrtion-img/09-2.jpg"
+			],
+			"like": 1
+		},
+		{
+			"title": "THE DOORS",
+			"date": "2018.11.11",
+			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
+			"imgSrc": "data/illustrtion-img/s10.jpg",
+			"detailSrc": [
+				"data/illustrtion-img/10-1.jpg",
+				"data/illustrtion-img/10-2.jpg"
+			],
+			"like": 1
+		},
+		{
+			"title": "ALIEN SPECIES",
+			"date": "2018.11.08",
+			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
+			"imgSrc": "data/illustrtion-img/s11.jpg",
+			"detailSrc": [
+				"data/illustrtion-img/11.jpg"
+			],
+			"like": 1
+		},
+		{
+			"title": "MAZDA",
+			"date": "2018.11.17",
+			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
+			"imgSrc": "data/illustrtion-img/s12.jpg",
+			"detailSrc": [
+				"data/illustrtion-img/12.jpg"
+			],
+			"like": 1
+		},
+		{
+			"title": "DANCE TEACHERS",
+			"date": "2018.11.27",
+			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
+			"imgSrc": "data/illustrtion-img/s13.jpg",
+			"detailSrc": [
+				"data/illustrtion-img/13-1.jpg",
+				"data/illustrtion-img/13-2.jpg",
+				"data/illustrtion-img/13-3.jpg"
+			],
+			"like": 1
+		},
+		{
+			"title": "EARPHONES",
+			"date": "2018.12.17",
+			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
+			"imgSrc": "data/illustrtion-img/s14.jpg",
+			"detailSrc": [
+				"data/illustrtion-img/14-1.jpg",
+				"data/illustrtion-img/14-2.jpg",
+				"data/illustrtion-img/14-3.jpg"
+			],
+			"like": 1
+		},
+		{
+			"title": "CHRISTMAS",
+			"date": "2018.12.24",
+			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
+			"imgSrc": "data/illustrtion-img/s15.jpg",
+			"detailSrc": [
+				"data/illustrtion-img/15-1.jpg",
+				"data/illustrtion-img/15-2.jpg"
+			],
+			"like": 1
+		},
+		{
+			"title": "2019 NEW YEAR",
+			"date": "2018.12.31",
+			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
+			"imgSrc": "data/illustrtion-img/s16.jpg",
+			"detailSrc": [
+				"data/illustrtion-img/16-1.jpg",
+				"data/illustrtion-img/16-2.jpg"
+			],
+			"like": 1
+		},
+		{
+			"title": "THE YEAR OF PIG",
+			"date": "2018.02.04",
+			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
+			"imgSrc": "data/illustrtion-img/s17.jpg",
+			"detailSrc": [
+				"data/illustrtion-img/17.jpg"
 			],
 			"like": 1
 		}
@@ -184,10 +314,10 @@ let SETTING={
 			"title": "THE BOTTLES 1",
 			"date": "2018.06.11",
 			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
-			"imgSrc": "data/img/20190101.png",
+			"imgSrc": "data/img/20190101.jpg",
 			"detailSrc": [
-				"data/img/3-1.png",
-				"data/img/3-2.png"
+				"data/img/3-1.jpg",
+				"data/img/3-2.jpg"
 			],
 			"like": 0
 		},
@@ -195,9 +325,9 @@ let SETTING={
 			"title": "THE BOTTLES 2",
 			"date": "2018.06.11",
 			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
-			"imgSrc": "data/img/20190102.png",
+			"imgSrc": "data/img/20190102.jpg",
 			"detailSrc": [
-				"data/img/4-1.png"
+				"data/img/4-1.jpg"
 			],
 			"like": 1
 		},
@@ -205,10 +335,10 @@ let SETTING={
 			"title": "THE BOTTLES 3",
 			"date": "2018.06.11",
 			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
-			"imgSrc": "data/img/20190101.png",
+			"imgSrc": "data/img/20190101.jpg",
 			"detailSrc": [
-				"data/img/3-1.png",
-				"data/img/3-2.png"
+				"data/img/3-1.jpg",
+				"data/img/3-2.jpg"
 			],
 			"like": 7
 		},
@@ -216,9 +346,9 @@ let SETTING={
 			"title": "THE BOTTLES 4",
 			"date": "2018.06.11",
 			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
-			"imgSrc": "data/img/20190102.png",
+			"imgSrc": "data/img/20190102.jpg",
 			"detailSrc": [
-				"data/img/4-1.png"
+				"data/img/4-1.jpg"
 			],
 			"like": 2
 		},
@@ -226,10 +356,10 @@ let SETTING={
 			"title": "THE BOTTLES 5",
 			"date": "2018.06.11",
 			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
-			"imgSrc": "data/img/20190101.png",
+			"imgSrc": "data/img/20190101.jpg",
 			"detailSrc": [
-				"data/img/3-1.png",
-				"data/img/3-2.png"
+				"data/img/3-1.jpg",
+				"data/img/3-2.jpg"
 			],
 			"like": 4
 		},
@@ -237,10 +367,10 @@ let SETTING={
 			"title": "THE BOTTLES 6",
 			"date": "2018.06.11",
 			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
-			"imgSrc": "data/img/20190102.png",
+			"imgSrc": "data/img/20190102.jpg",
 			"detailSrc": [
-				"data/img/4-1.png",
-				"data/img/2-1.png"
+				"data/img/4-1.jpg",
+				"data/img/2-1.jpg"
 			],
 			"like": 0
 		},
@@ -248,10 +378,10 @@ let SETTING={
 			"title": "THE BOTTLES 3",
 			"date": "2018.06.11",
 			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
-			"imgSrc": "data/img/20190101.png",
+			"imgSrc": "data/img/20190101.jpg",
 			"detailSrc": [
-				"data/img/3-1.png",
-				"data/img/3-2.png"
+				"data/img/3-1.jpg",
+				"data/img/3-2.jpg"
 			],
 			"like": 7
 		},
