837bbc0a0494d88b3df1ce8a0a20fcbb06d28005
511 <a511@511deMacBook-Pro.local>
2019-02-25 20:43:39 +0800

    B

--- data/conf.js
+++ data/conf.js
@@ -1,112 +1,215 @@
 let SETTING={
 	"homelist": [
 		{
-			"title": "THE BOTTLES 1",
-			"date": "2018.06.11",
-			"description": "Lorem ipsum dolor sit amet, <a href@http://www.baidu.com>百度</a> consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
-			"imgSrc": "data/img/20190101.png",
-			"detailSrc": [
-				"data/img/3-1.png",
-				"data/img/3-2.png"
-			],
-			"like": 9
-		},
-		{
-			"title": "THE BOTTLES 2",
-			"date": "2018.06.11",
+			"title": "2019 NEW YEAR",
+			"date": "2018.12.31",
 			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
-			"imgSrc": "data/img/20190102.png",
+			"imgSrc": "data/illustration-img/s16.jpg",
 			"detailSrc": [
-				"data/img/4-1.png"
-			],
-			"like": 5
-		},
-		{
-			"title": "THE BOTTLES 3",
-			"date": "2018.06.11",
-			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
-			"imgSrc": "data/img/20190101.png",
-			"detailSrc": [
-				"data/img/3-1.png",
-				"data/img/3-2.png"
-			],
-			"like": 8
-		},
-		{
-			"title": "THE BOTTLES 4",
-			"date": "2018.06.11",
-			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
-			"imgSrc": "data/img/20190102.png",
-			"detailSrc": [
-				"data/img/4-1.png"
-			],
-			"like": 3
-		},
-		{
-			"title": "THE BOTTLES 5",
-			"date": "2018.06.11",
-			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
-			"imgSrc": "data/img/20190101.png",
-			"detailSrc": [
-				"data/img/3-1.png",
-				"data/img/3-2.png"
-			],
-			"like": 4
-		},
-		{
-			"title": "THE BOTTLES 6",
-			"date": "2018.06.11",
-			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
-			"imgSrc": "data/img/20190102.png",
-			"detailSrc": [
-				"data/img/4-1.png",
-				"data/img/2-1.png"
-			],
-			"like": 3
-		}
-	],
-	"uilist": [
-		{
-			"title": "THE BOTTLES 1",
-			"date": "2018.06.11",
-			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
-			"imgSrc": "data/img/20190101.png",
-			"detailSrc": [
-				"data/img/3-1.png",
-				"data/img/3-2.png"
-			],
-			"like": 5
-		},
-		{
-			"title": "THE BOTTLES 2",
-			"date": "2018.06.11",
-			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
-			"imgSrc": "data/img/20190102.png",
-			"detailSrc": [
-				"data/img/4-1.png",
-				"data/img/2-1.png"
+				"data/illustration-img/16-1.jpg",
+				"data/illustration-img/16-2.jpg"
 			],
 			"like": 1
 		},
 		{
-			"title": "THE BOTTLES 3",
-			"date": "2018.06.11",
+			"title": "ANIMATION",
+			"date": "2018.11.01",
 			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
-			"imgSrc": "data/img/20190101.png",
+			"imgSrc": "data/ui-img/06.jpg",
 			"detailSrc": [
-				"data/img/3-1.png",
-				"data/img/3-2.png"
+				"data/ui-img/06-1.gif",
+				"data/ui-img/06-2.gif",
+				"data/ui-img/06-3.gif"
+			],
+			"like": 2
+		},
+		{
+			"title": "WINWIN FINTECH",
+			"date": "2018.12.19",
+			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
+			"imgSrc": "data/ui-img/08.jpg",
+			"detailSrc": [
+				"data/ui-img/08-1.jpg",
+				"data/ui-img/08-2.jpg",
+				"data/ui-img/08-3.jpg",
+				"data/ui-img/08-4.jpg",
+				"data/ui-img/08-5.gif",
+				"data/ui-img/08-6.jpg",
+				"data/ui-img/08-7.gif",
+				"data/ui-img/08-8.jpg"
+			],
+			"like": 2
+		},
+		{
+			"title": "UNIONWIN INTERNATIONAL",
+			"date": "2018.08.11",
+			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
+			"imgSrc": "data/ui-img/04.jpg",
+			"detailSrc": [
+				"data/ui-img/04-1.jpg",
+				"data/ui-img/04-2.gif",
+				"data/ui-img/04-3.jpg",
+				"data/ui-img/04-4.jpg",
+				"data/ui-img/04-5.jpg",
+				"data/ui-img/04-6.jpg"
+			],
+			"like": 2
+		},
+		{
+			"title": "SUPERWIN COMICS",
+			"date": "2018.08.20",
+			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
+			"imgSrc": "data/illustration-img/s06.jpg",
+			"detailSrc": [
+				"data/illustration-img/06-1.jpg",
+				"data/illustration-img/06-2.jpg"
+			],
+			"like": 2
+		},
+		{
+			"title": "TRAVELING",
+			"date": "2018.09.17",
+			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
+			"imgSrc": "data/illustration-img/s07.jpg",
+			"detailSrc": [
+				"data/illustration-img/07-1.jpg",
+				"data/illustration-img/07-2.jpg",
+				"data/illustration-img/07-3.jpg",
+				"data/illustration-img/07-4.jpg"
+			],
+			"like": 1
+		},
+		{
+			"title": "EARPHONES",
+			"date": "2018.12.17",
+			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
+			"imgSrc": "data/illustration-img/s14.jpg",
+			"detailSrc": [
+				"data/illustration-img/14-1.jpg",
+				"data/illustration-img/14-2.jpg",
+				"data/illustration-img/14-3.jpg"
+			],
+			"like": 2
+		},
+		{
+			"title": "THE YEAR OF PIG",
+			"date": "2018.02.04",
+			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
+			"imgSrc": "data/illustration-img/s17.jpg",
+			"detailSrc": [
+				"data/illustration-img/17.jpg"
+			],
+			"like": 1
+		}
+	],
+	"uilist": [
+		{
+			"title": "DEBUIT&CREDIT",
+			"date": "2018.02.26",
+			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
+			"imgSrc": "data/img/s01.jpg",
+			"detailSrc": [
+				"data/ui-img/01-1.jpg",
+				"data/ui-img/01-2.jpg",
+				"data/ui-img/01-3.jpg"
+			],
+			"like": 5
+		},
+		{
+			"title": "ESTIMATE MASTER",
+			"date": "2018.03.09",
+			"description": "魔估大师是一款资产评估信息展示APP。对资产估价、资产竞得方、城市指数等进行评估整合，通过数据可视化的方式向用户展示,用户可通过账户登录收藏地址。</br>魔估大师APP提供了很多自定义方式供用户查看信息，包括自定义选择区域、排序等功能，提供更个性化的信息服务。",
+			"imgSrc": "data/ui-img/s02.jpg",
+			"detailSrc": [
+				"data/ui-img/02-1.jpg",
+				"data/ui-img/02-2.jpg",
+				"data/ui-img/02-3.jpg",
+				"data/ui-img/02-4.jpg",
+				"data/ui-img/02-5.jpg"
+			],
+			"like": 1
+		},
+		{
+			"title": "ICONS",
+			"date": "2018.7.10",
+			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
+			"imgSrc": "data/ui-img/03.jpg",
+			"detailSrc": [
+				"data/ui-img/03-1.jpg",
+				"data/ui-img/03-2.jpg",
+				"data/ui-img/03-3.jpg"
 			],
 			"like": 7
 		},
 		{
-			"title": "THE BOTTLES 4",
-			"date": "2018.06.11",
+			"title": "UNIONWIN INTERNATIONAL",
+			"date": "2018.08.11",
 			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
-			"imgSrc": "data/img/20190102.png",
+			"imgSrc": "data/ui-img/04.jpg",
 			"detailSrc": [
-				"data/img/4-1.png",
-				"data/img/2-1.png"
+				"data/ui-img/04-1.jpg",
+				"data/ui-img/04-2.gif",
+				"data/ui-img/04-3.jpg",
+				"data/ui-img/04-4.jpg",
+				"data/ui-img/04-5.jpg",
+				"data/ui-img/04-6.jpg"
+			],
+			"like": 2
+		},
+		{
+			"title": "OPERATING ACTIVITIES",
+			"date": "2018.10.20",
+			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
+			"imgSrc": "data/ui-img/05.jpg",
+			"detailSrc": [
+				"data/ui-img/05-1.gif",
+				"data/ui-img/05-2.jpg",
+				"data/ui-img/05-3.jpg",
+				"data/ui-img/05-4.jpg"
+			],
+			"like": 2
+		},
+		{
+			"title": "ANIMATION",
+			"date": "2018.11.01",
+			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
+			"imgSrc": "data/ui-img/06.jpg",
+			"detailSrc": [
+				"data/ui-img/06-1.gif",
+				"data/ui-img/06-2.gif",
+				"data/ui-img/06-3.gif"
+			],
+			"like": 2
+		},
+		{
+			"title": "DEFAULT PAGE",
+			"date": "2018.11.20",
+			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
+			"imgSrc": "data/ui-img/07.jpg",
+			"detailSrc": [
+				"data/ui-img/07-1.gif",
+				"data/ui-img/07-2.gif",
+				"data/ui-img/07-3.gif",
+				"data/ui-img/07-4.gif",
+				"data/ui-img/07-5.gif",
+				"data/ui-img/07-6.gif"
+			],
+			"like": 2
+		},
+		{
+			"title": "WINWIN FINTECH",
+			"date": "2018.12.19",
+			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
+			"imgSrc": "data/ui-img/08.jpg",
+			"detailSrc": [
+				"data/ui-img/08-1.jpg",
+				"data/ui-img/08-2.jpg",
+				"data/ui-img/08-3.jpg",
+				"data/ui-img/08-4.jpg",
+				"data/ui-img/08-5.gif",
+				"data/ui-img/08-6.jpg",
+				"data/ui-img/08-7.gif",
+				"data/ui-img/08-8.jpg"
 			],
 			"like": 2
 		}
@@ -389,10 +492,10 @@ let SETTING={
 			"title": "THE BOTTLES 4",
 			"date": "2018.06.11",
 			"description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.",
-			"imgSrc": "data/img/20190102.png",
+			"imgSrc": "data/img/20190102.jpg",
 			"detailSrc": [
-				"data/img/4-1.png",
-				"data/img/2-1.png"
+				"data/img/4-1.jpg",
+				"data/img/2-1.jpg"
 			],
 			"like": 2
 		}
